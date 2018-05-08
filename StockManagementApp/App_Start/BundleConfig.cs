625531462f910165c951c410bcf9c81cf7e039d3
shihabrpi15 <shihabrpi15@yahoo.com>
2018-05-09 02:12:31 +0600

    Many file edit

---
+++ StockManagementApp/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace StockManagementApp.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                            "~/js/jquery.js"));
+        }
+    }
+}
